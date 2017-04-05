543a2f2e6c25290543a934438e91ed50def9eb22
bmoby <msouleymanov@gmail.com>
2017-04-05 12:00:21 +0200

    fjkdshfs

--- app.js
+++ app.js
@@ -81,8 +81,6 @@ app.use('/users', users);
 // Set Port
 app.set('port', (process.env.PORT || 3000));
 
-var server = app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
 });
-
-server.timeout = 500;
