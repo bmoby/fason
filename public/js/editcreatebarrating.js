263e0e11f8e510535f4b548a259f1faa5fc0dcdf
bmoby <msouleymanov@gmail.com>
2017-04-06 12:48:29 +0200

    fkjdshfsk

---
+++ public/js/editcreatebarrating.js
@@ -0,0 +1,12 @@
+$(document).ready(function{
+  $('.rating').each(function(){
+    var styleboxId = this.getAttribute("data-stylebox-id");
+    var ratingValue = this.getAttribute("data-rating-value");
+
+    $('.general-rating-'+styleboxId).barrating({
+      theme: "fontawesome-stars-o",
+      initialRating: ratingValue,
+      readonly: true
+    });
+  })
+})
