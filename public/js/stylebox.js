ca7d50e22ff36b42a90f934c85be2c00d0953866
bmoby <msouleymanov@gmail.com>
2017-04-10 18:52:47 +0200

    dlksqjldjq

--- public/js/stylebox.js
+++ public/js/stylebox.js
@@ -119,6 +119,7 @@ $(document).ready(function(){
       data: JSON.stringify({"message": message, "styleboxId": styleboxId}),
       success: function(response){
         if (response){
+          $('.messageText').val("");
           alert("Votre message a bien été envoyé. Vous recevrez une notification en cas de réponse.");
           $('.full-page').addClass('hidden');
         }
