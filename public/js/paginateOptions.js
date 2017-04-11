3878ff5ba88a56bd860e9758b48549d1de225644
bmoby <msouleymanov@gmail.com>
2017-04-11 23:22:04 +0200

    kfjsdjkfksdh

--- public/js/paginateOptions.js
+++ public/js/paginateOptions.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-  $('#listitems').paginate({itemsPerPage: 5});
-  $('.next').on('click', function(){
-    window.scrollTo(0, 0);
+
+  // var looksCount = ($('#listitems li').size()/4.01).toFixed(2);
+  var looksCount = $('#listitems li').size();
+
+  function sizestyleboxes(count){
+    if(count%5 == 0){
+      return count/5
+    }else{
+      return parseInt((count/5) +1);
+    }
+  }
+
+  function showmaxpage(nombre){
+    if(nombre >= 3){
+      return 3;
+    } else {
+      return nombre;
+    }
+  }
+  // alert(showmaxpage(sizestyleboxes(looksCount)))
+  $('#listitems').paginathing({
+    perPage:5,
+    limitPagination: showmaxpage(sizestyleboxes(looksCount)),
+    firstText: '1',
+    lastText: sizestyleboxes(looksCount),
+    insertAfter: null,
   });
-  $('.previous').on('click', function(){
-    window.scrollTo(0, 0);
-  })
+
 });
